e63c7ad38066c9060ec54ac3b9f850ab566e1def
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2024-08-20 10:44:43 +0700

    feat: finishing demo day 3

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("renders the counter app", () => {
   render(<App />);
